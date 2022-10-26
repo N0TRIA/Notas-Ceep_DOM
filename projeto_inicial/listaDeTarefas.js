995d601960ed83377ec089a186032ff3226e7f38
N0TRIA <airtonlameraaliivl@gmail.com>
2022-10-25 22:51:30 -0300

    Criando botão-deletar

--- projeto_inicial/listaDeTarefas.js
+++ projeto_inicial/listaDeTarefas.js
@@ -15,6 +15,7 @@
             tarefa.innerHTML = conteudo;
 
             tarefa.appendChild(botaoConclui())
+            tarefa.appendChild(BotaoDeleta())
             lista.appendChild(tarefa);
             input.value = " ";
         }
@@ -35,11 +36,32 @@
         }
 
         const concluirTarefa = (evento) => {
+
             const botaoConclui = evento.target;
 
             const tarefaCompleta = botaoConclui.parentElement;
 
             tarefaCompleta.classList.toggle('done');
+        }
+
+        const BotaoDeleta = () => {
+
+            const botaoDeleta = document.createElement('button');
+            
+            botaoDeleta.classList.add('delete-button');
+            botaoDeleta.innerText = 'deletar';
+            botaoDeleta.addEventListener('click', deletarTarefa);
+
+            return botaoDeleta;
+        }
+
+        const deletarTarefa = (evento) => {
+
+            const botaoDeleta = evento.target;
+            const tarefaCompleta = botaoDeleta.parentElement;
+            tarefaCompleta.remove();
+
+            return botaoDeleta
         }
     }
 )()
